4b79e5d6d4a14e689481e19a4181ab2a36bdc70b
kieranford4805 <kieran.ford@ufl.edu>
2022-12-03 14:02:15 -0500

    Implemented unit tests in Counter.test.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,10 +10,10 @@ export default function Counter() {
   }
   return (
     <div>
-      <h2>Counter</h2>
+      <h2 data-testid="Counter">Counter</h2>
       <p data-testid="count">{count}</p>
-      <button onClick={increment}> + </button>
-      <button onClick={decrement}> - </button>
+      <button data-testid = "addBtn" onClick={increment}> + </button>
+          <button data-testid="subBtn" onClick={decrement}> - </button>
     </div>
   );
 }
